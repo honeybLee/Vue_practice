363a1609c43e9e842f48ed92b6dd5b64e38b56b3
honeybLee <67769117+honeybLee@users.noreply.github.com>
2024-01-03 21:04:18 +0900

    5강 완료

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -1,17 +1,18 @@
+<!-- =========================== Vue =========================== -->
 <template>
-  <div class="black-bg">
-    <div class="white-gb">
-      <h4>상세페이지임</h4>
+  <!-- Modal Begin -->
+  <div class="black-bg" v-if="modal_isopen == true">
+    <div  class="white-bg">
+      <h4> 상세페이지임</h4>
       <p>상세페이지 내용임</p>
+      <button @click="modal_isopen=false"> Close </button>
     </div>
   </div>
-
-
-
-
+  <!-- Modal End -->
 
   <h3>원룸샵 ^ㅅ^</h3>
   <img alt="Vue logo" src="./assets/logo.png">
+
 
   <!-- Menu Bar -->
   <div class="menu">
@@ -30,7 +31,7 @@
   <!--Event Handler Begin-->
   <div>
     <img src="./assets/room0.jpg" class="room-img">
-    <h4>{{ products[0] }}</h4>
+    <h4 id="product_0" @click="modal_isopen = true">{{ products[0] }}</h4>
     <p> 50만원 </p>
     <button @click="click_report[0] += 1"> 허위매물 신고 </button> <span>신고수: {{ click_report[0] }}</span>
   </div>
@@ -50,17 +51,19 @@
 </template>
 
 
+<!-- =========================== JavaScript =========================== -->
 <script>
 export default {
   name: 'App',
-  // Data 보관함 
+  // Data 보관함
+  // UI 의 상태를 보관 (React에선s Status 라고 함)
   data() {
     return {
-
-      prices: [40, 50, 60],
       products: ['역삼동 원룸', '천호동 원룸', '마포구 원룸'],
+      prices: [40, 50, 60],
       menu_array: ['Home', 'Products', 'About'],
-      click_report: [0, 0, 0]
+      click_report: [0, 0, 0],
+      modal_isopen: false,
     }
   },
 
@@ -80,7 +83,33 @@ export default {
 
 
 
+<!-- =========================== CSS =========================== -->
 <style>
+/* modal begin */
+body {
+  margin: 0
+}
+
+div {
+  box-sizing: border-box;
+}
+
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  padding: 20px;
+}
+
+.white-bg {
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+/* modal end */
+
 .room-img {
   width: 50%;
   margin-top: 40px;
