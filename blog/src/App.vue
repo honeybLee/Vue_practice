de8e0324cba8faefcf98c6499ef81fb17ef80a96
honeybLee <67769117+honeybLee@users.noreply.github.com>
2024-01-15 14:53:16 +0900

    router practice begin

---
+++ blog/src/App.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="container mt-4 mb-4">
+    <h2> Vue Practice Blog </h2>
+  </div>
+
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">HoneyBlog</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Dropdown
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Action</a></li>
+              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li>
+                <hr class="dropdown-divider">
+              </li>
+              <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+          </li>
+        </ul>
+        <form class="d-flex" role="search">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
+    </div>
+  </nav>
+
+  <COM_List :blog_data="blog_data[i]" v-for="(data, i) in blog_data" :key="data" />
+</template>
+
+
+
+<script>
+import List from './components/List.vue';
+import blog from './assets/blog_data.js';
+
+export default {
+  name: 'App',
+
+  data() {
+    return {
+      blog_data: blog,
+    }
+  },
+
+  components: {
+    COM_List: List,
+  }
+}
+</script>
+
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ blog/src/components/List.vue
@@ -0,0 +1,28 @@
+<template lang="english">
+<div style="display: flex; align-items: center; justify-content: center;">
+<div class="card" style="width: 35rem; margin: 20px">
+  <div class="card-body">
+    <h5 class="card-title">{{blog_data.title}}</h5>
+    <h6 class="card-subtitle mb-2 text-body-secondary">{{blog_data.date}}</h6>
+    <p class="card-text">{{blog_data.content}}</p>
+    <a href="#" class="card-link">{{blog_data.number}}_link</a>
+  </div>
+  </div>
+</div>
+
+</template>
+<script>
+export default {
+  name: 'com_list',
+  data() {
+    return {
+    }
+  },
+  props: {
+    blog_data: Object
+  },
+}
+</script>
+<style lang="">
+    
+</style>
