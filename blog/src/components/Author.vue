d20aca75d7064a464ec641842bd13afd8aa4a231
honeybLee <67769117+honeybLee@users.noreply.github.com>
2024-01-15 18:24:54 +0900

    component add

---
+++ blog/src/components/Author.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <p> 작성자임 </p>
+    </div>
+</template>
+<script>
+export default {
+    name: 'com_author',
+    data() {
+        return {
+        }
+    },
+
+}
+</script>
+<style></style>
---
+++ blog/src/components/Comment.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <p> 댓글임 </p>
+    </div>
+</template>
+<script>
+export default {
+    name: 'com_comment',
+    data() {
+        return {
+        }
+    },
+
+}
+</script>
+<style>
+    
+</style>
